e8cb7bb493241e30909579ed6bc658003f8993c6
oguzhan-peker <peker.oguzhan1634@gmail.com>
2023-04-05 14:36:39 +0300

    ders1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-
+ /*
         System.out.println("Merhaba Java");
         System.out.println("Merhaba Java 2 ");
+        // ders
+        int ogrenciSayisi = 13;
+        String mesaj = "Öğrenci sayisi : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+        // ders degisti
+        int sayi = 12;
+        double sayii = 12.5;
+        char karakter = 'A';
+        boolean dogruMu = false;
+        // ders gecisi
+        int sayi = 21;
+        if(sayi<20){
+            System.out.println("Sayı 20 den küçüktür");
+        }
+        else if(sayi==20){
+            System.out.println("Sayı 20'ye eşittir");
+        }
+        else{
+            System.out.println("Sayı 20 den büyüktür");
+
+  */
+
+
     }
 }
